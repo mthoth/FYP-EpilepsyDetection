b8f7ea9406517c93f6af62b63e8df49e1a5f7316
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2022-04-03 11:11:45 +0300

    Refactor: ♻ EDFtoCSV.py to work on any patient
    - Moved 🚚 it and its helpers into its own folder

---
+++ EDFtoCSV/EDFtoCSV.py
@@ -0,0 +1,49 @@
+import pyedflib
+import pandas as pd
+import helpers as hp
+
+
+""" Please make sure to have a folder outside the repo as follows: 
+    'Dataset/chbxx/' (xx = patient no.) 
+    and 'Dataset/Dataset_Excelsheets/'.
+    ---
+    Make sure YOU CREATE THE FOLDERS before running this module.
+"""
+
+
+def convertPatientFiles(prefix, patientDir):
+    fullPath = f'{prefix}/{patientDir}'
+    destinationPath = f'{prefix}/Dataset_Excelsheets/{patientDir}'
+
+    filesInDir, dirLength = hp.getFilesInDirectory(fullPath)
+    print(f'Entering directory: {fullPath} ---- dirLength: {dirLength} \n')
+
+    for currFile in filesInDir:
+        if not currFile.endswith('.edf'):
+            continue
+
+        filePath = r'{}/{}'.format(fullPath, currFile)
+        print(f'Reading {currFile}...', end="\n\t")
+
+        rawEdf = pyedflib.EdfReader(filePath)
+        rawDf = pd.DataFrame()
+        signals = rawEdf.getSignalLabels()
+        uniqueSignals = set(signals)
+
+        hp.appendSignalsToDataframe(uniqueSignals, rawDf, rawEdf)
+        print(f'Converted edf to dataframe shape:{rawDf.shape}...', end="\n\t")
+
+        csvName = f'{currFile[:8]}.csv'
+        hp.convertDataframeToCsv(rawDf, csvName, uniqueSignals)
+        hp.moveFileToDestination(src=csvName, dest=destinationPath)
+    print(f'patient {patientDir} complete')
+
+
+pathPrefix = r'../Dataset'
+# These should be changed to YOUR specific patients
+patientDirs = (r'chb18', r'chb19', r'chb20', r'chb21', r'chb22')
+
+for currPatientDir in patientDirs:
+    convertPatientFiles(pathPrefix, currPatientDir)
+
+print('FILE CONVERSIONS COMPLETE!')
---
+++ EDFtoCSV/helpers.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+
+def getFilesInDirectory(path):
+    listOfFiles = os.listdir(path)
+    return (listOfFiles, len(listOfFiles))
+
+
+def convertDataframeToCsv(dataframe, targetName, headerNames):
+    print(f'Converting to final csv...', end="\n\t")
+    dataframe.to_csv(targetName, header=headerNames)
+    print('conversion done.')
+
+
+def appendSignalsToDataframe(signals, dataframe, edf):
+    def isAValidSignalName(sigName):
+        return sigName[0].isalpha() and i < 18
+
+    i = 0
+    for currSignal in signals:
+        if isAValidSignalName(currSignal):
+            dataframe.insert(i, currSignal, edf.readSignal(i))
+            i += 1
+
+
+def moveFileToDestination(src, dest):
+    destinationDir = r'{}/{}'.format(dest, src)
+    try:
+        if not os.path.exists(destinationDir):
+            os.makedirs(destinationDir)
+
+        shutil.move(src, destinationDir)
+        print(f'successfully saved to {destinationDir}.\n')
+    except:
+        print('Moving file failed.')
